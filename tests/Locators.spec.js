e2799940c9e7e1c34b0cc9b73eb03b638ef466a3
Julio Uribe <uribejulioc@gmail.com>
2024-01-11 00:00:14 -0800

    Add locators tests for login process

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -7,5 +7,21 @@ test('Locators', async ({ page }) => {
   // await page.locator('id=login2').click();
   await page.click('id=login2');
 
-  // 
+  // provide username - CSS
+  // await page.locator('#loginusername').fill('pavanol');
+  await page.fill('#loginusername', 'pavanol');
+  // await page.type('#loginusername', 'pavanol');
+
+  // provide password - CSS
+  await page.fill("input[id='loginpassword']", 'test@123');
+
+  // Click on login button
+  await page.click("//button[normalize-space(.)='Log in']");
+
+  // Verify logout link presence
+  const logoutLink = await page.locator("//a[normalize-space(.)='Log out']");
+
+  await expect(logoutLink).toBeVisible();
+
+  await page.close();
 });
